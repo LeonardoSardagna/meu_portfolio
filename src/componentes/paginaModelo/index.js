3b38a21dd81d2dfccd4d69f265f0a82d7dd7603f
Leonardo Sardagna <leonardosardagna00@gmail.com>
2024-03-19 21:08:56 -0300

    Projeto Finalizado

--- src/componentes/paginaModelo/index.js
+++ src/componentes/paginaModelo/index.js
@@ -1,11 +1,10 @@
 import styles from "../../componentes/paginaModelo/modelo.module.css"
 
-export default function Paginamodelo({fotoCapa, titulo, children}){
+export default function Paginamodelo({titulo, children}){
     return(
         <article className={styles.container}>
             <div
             className={styles.fotoCapa}
-            style={{backgroundImage: `url(${fotoCapa})`}}
             ></div>
             <h2 className={styles.titulo}>{titulo}</h2>
             <div className={styles.paragrafo}>{children}</div>
--- src/componentes/postCard/index.js
+++ src/componentes/postCard/index.js
@@ -28,7 +28,6 @@ export default function PostCard(){
             <Route path="*" element={<PaginaPadrao/>}>
                 <Route index element={
                     <Paginamodelo
-                    fotoCapa={`public/assets/posts/${post.id}/capa.png`}
                     titulo={post.titulo}
                     >
                     <div className={styles.post_markdown_container}>
