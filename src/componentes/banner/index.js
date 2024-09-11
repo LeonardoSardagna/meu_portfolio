5830dde1e93b5cff3aa405676b6f03e4e3abc173
Leonardo Sardagna <leonardosardagna00@gmail.com>
2024-09-11 18:58:36 -0300

    Alteração da bio

--- src/componentes/banner/index.js
+++ src/componentes/banner/index.js
@@ -8,7 +8,7 @@ export default function Banner() {
             <section className={styles.banner}>
                 <div className={styles.info}>
                     <h1>Olá, mundo!</h1>
-                    <p>Meu nome é Leonardo, iniciei minha trajetória na tecnologia em 2023, cursando <strong>Análise e Desenvolvimento de Sistemas</strong>. Desenvolvi um grande interesse pelo desenvolvimento web, aprofundando-me em <strong>HTML, CSS, JavaScript, React, TypeScript, Java , Spring e Banco de dados</strong>. Focado no desenvolvimento full-stack, busco dominar tanto front-end quanto back-end para criar soluções completas e eficientes. Confira alguns dos meus projetos:</p>
+                    <p>Meu nome é Leonardo, iniciei minha trajetória na tecnologia em 2023, cursando <strong>Análise e Desenvolvimento de Sistemas</strong>. Desenvolvi um grande interesse pelo desenvolvimento, aprofundando-me em <strong>Java, Spring, SQL, HTML, CSS, JavaScript, React e TypeScript</strong>. Focado no desenvolvimento com Java e Cloud Computing, busco dominar tecnologias robustas para criar soluções escaláveis e eficientes. Confira alguns dos meus projetos:</p>
                 </div>
                 <div className={styles.imagem}>
                     <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/06f21a161921919.63cd7887d0a70.gif" alt=""></img>
