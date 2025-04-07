9e35ced4b39f9902218bde914d7507ebb792ca2c
Leonardo Sardagna <leonardosardagna00@gmail.com>
2025-04-06 21:04:15 -0300

    atualizando bio e currículo

--- src/componentes/banner/index.js
+++ src/componentes/banner/index.js
@@ -8,7 +8,9 @@ export default function Banner() {
             <section className={styles.banner}>
                 <div className={styles.info}>
                     <h1>Olá, mundo!</h1>
-                    <p>Meu nome é Leonardo, e desde 2023 venho construindo uma base sólida em tecnologia com o curso de Análise e Desenvolvimento de Sistemas. Atualmente, sou estagiário de Infraestrutura de TI na T-Systems Brasil, com conhecimentos em <strong>Windows Server, Linux, Docker, Bancos de Dados</strong> e <strong>Cloud Computing (AWS e Azure)</strong>. Estou em constante evolução, buscando me aprofundar cada vez mais em infraestrutura de TI para oferecer soluções eficientes e seguras.</p>
+                    <p>Sou estudante de Análise e Desenvolvimento de Sistemas e atuo como Estagiário de Infraestrutura de TI na T-Systems Brasil. Atualmente, estou em processo de aprendizagem em <strong>SAP Basis Administration</strong> e <strong>SAP HANA</strong>, além de possuir sólidos conhecimentos em <strong>bancos de dados SQL e PostgreSQL</strong>, e desenvolvimento com <strong>Java e Spring Boot</strong>. Estou aprofundando meus estudos em ambientes <strong>Linux e Windows</strong>, assim como em infraestrutura de TI de forma geral.</p>
+
+                    <p>Minha passagem pelo <strong>Exército Brasileiro</strong> me proporcionou valores essenciais como determinação, disciplina e resiliência — princípios que aplico diariamente no ambiente de TI para lidar com desafios de forma proativa e comprometida.</p>
                 </div>
                 <div className={styles.imagem}>
                     <img src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/06f21a161921919.63cd7887d0a70.gif" alt=""></img>
